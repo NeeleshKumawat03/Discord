a00a5f537185b7693b4027f60a05139f364e04f2
Neelesh Kumawat <neeleshkumawat03@gmail.com>
2024-08-26 19:32:10 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitnet:["Whitney"],
+    Gint0:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
